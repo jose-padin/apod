d99d36e7e5f1bdfb1765d83f484ca77e1adb267e
jose.padin <jose.mendez@kubicum.com>
2022-01-31 21:05:29 +0100

    Added support to NASA rovers photos too

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import requests
 from datetime import datetime as dt
 from pprint import PrettyPrinter
@@ -16,8 +17,12 @@ TWITTER_API_KEY_SECRET = config.get("TWITTER_API_KEY_SECRET")
 TWITTER_ACCESS_TOKEN = config.get("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = config.get("TWITTER_ACCESS_TOKEN_SECRET")
 
+nasa_base_url = "https://api.nasa.gov"
+ROVER_CAMERAS = ["FHAZ", "RHAZ", "MARDI", "NAVCAM", "PANCAM"]
 APOD = "Astronomy Picture of the Day"
-apod_url = "https://api.nasa.gov/planetary/apod"
+apod_url = f"{nasa_base_url}/planetary/apod"
+rovers_photos_url = f"{nasa_base_url}/mars-photos/api/v1/rovers/curiosity/photos"
+
 
 # Tweepy authentication
 auth=tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
@@ -27,41 +32,105 @@ twitter_api=tweepy.API(auth)
 # NOTE: consumer_secret == api_key_secret
 
 
-def main():
+
+def main(api, cam):
     # NOTE: downloading pic from NASA
     payload = {}
     count = 0
-    while "hdurl" not in payload:
-        payload = requests.get(f"{apod_url}?api_key={NASA_API_KEY}").json()
-        count += 1
-        if count >= 5:
-            break
+    error = False
+    sol = random.randrange(1000)
 
-    image_url = payload.get("hdurl", None)
-    image = requests.get(f"{image_url}?api_key={NASA_API_KEY}")
-    file_extension = image_url[image_url.rfind("."):]
+    if api == 0:
+        while "hdurl" not in payload:
+            try:
+                payload = requests.get(f"{apod_url}?api_key={NASA_API_KEY}").json()
+            except Exception as e:
+                error = True
+                print(f"Error: {e}")
 
-    # NOTE: saving pic in a file. Could be skipped
-    if image:
-        title = payload.get("title", str(dt.now()))
-        path = f"images/{title}{file_extension}"
-        with open(path, "wb") as f:
-            f.write(image.content)
+            count += 1
+            if count >= 3:
+                break
 
-        # NOTE: uploading pic to Twitter
-        response = twitter_api.media_upload(path)
-        media_id = response.media_id
-        copyright = payload.get("copyright", None)
-        message = title
-        message += f"\nSource: NASA ({APOD})\n"
-        tags = "\n#nasa #apod #photo"
-        if copyright:
-            message += f"Copyright: {copyright}"
+        image_url = payload.get("hdurl", None)
 
-        message += tags
+        try:
+            image = requests.get(f"{image_url}?api_key={NASA_API_KEY}")
+        except Exception as e:
+            error = True
+            print(f"Error 2: {e}")
 
-        twitter_api.update_status(status=message, media_ids=[media_id])
+        if not error:
+
+            file_extension = image_url[image_url.rfind("."):]
+
+            # NOTE: saving pic in a file. Could be skipped
+            if image:
+                title = payload.get("title", str(dt.now()))
+                path = f"images/{title}{file_extension}"
+                with open(path, "wb") as f:
+                    f.write(image.content)
+
+                # NOTE: uploading pic to Twitter
+                response = twitter_api.media_upload(path)
+                media_id = response.media_id
+                copyright = payload.get("copyright", None)
+                message = title
+                message += f"\nSource: NASA ({APOD})\n"
+                tags = "\n#nasa #apod #photo"
+                if copyright:
+                    message += f"Copyright: {copyright}"
+
+                message += tags
+
+                twitter_api.update_status(status=message, media_ids=[media_id])
+    else:
+        cam = ROVER_CAMERAS[cam]
+        payload = requests.get(f"{rovers_photos_url}?api_key={NASA_API_KEY}&camera={cam}&sol={sol}").json()
+        rover_name = None # photos[0].rover.name
+        camera_name = None # photos[0].camera.full_name
+        img_src = None # photos[0].img_src
+        image = None
+        date = None
+        # sol = arriba
+        if payload and payload.get("photos", []):
+            for item in payload.get("photos", {}):
+                rover_name = item.get("rover", {}).get("name", "")
+                camera_name = item.get("camera", {}).get("full_name", "")
+                img_src = item.get("img_src", None)
+                date = item.get("earth_date", "")
+                break
+
+            try:
+                img_src = img_src.replace("http://mars.jpl.nasa.gov/", "https://mars.nasa.gov/")
+                image = requests.get(img_src)
+            except Exception as e:
+                error = True
+                print(f"Error: {e}")
+
+        if not error and image:
+            file_extension = img_src[img_src.rfind("."):]
+            path = f"images/{rover_name}-{camera_name}-{date}-sol_{sol}{file_extension}"
+            with open(path, "wb") as f:
+                f.write(image.content)
+
+            # NOTE: uploading pic to Twitter
+            response = twitter_api.media_upload(path)
+            media_id = response.media_id
+            copyright = payload.get("copyright", None)
+            message = camera_name
+            message += f"\nSource: NASA - ROVER ({rover_name})\n"
+            message += f"Date: {date} - Sol: {sol}\n"
+            tags = "\n#nasa #apod #photo #rover"
+            if copyright:
+                message += f"Copyright: {copyright}"
+
+            message += tags
+
+            twitter_api.update_status(status=message, media_ids=[media_id])
 
 
 if __name__ == "__main__":
-    main()
+    api_type = random.randrange(2)
+    cam_type = random.randrange(5)
+    main(api_type, cam_type)
